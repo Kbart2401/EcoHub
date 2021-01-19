c8171dd9b2b6598447e3040d62052c6ac8d66d71
Kyle Barthelmes <kbart2401@gmail.com>
2021-01-18 22:35:43 -0500

    finished air quality modal

--- frontend/src/components/modals/AirModal.js
+++ frontend/src/components/modals/AirModal.js
@@ -6,7 +6,8 @@ import {
   ModalHeader,
   ModalFooter,
   ModalBody,
-  ModalCloseButton, Button, useDisclosure, Link
+  ModalCloseButton, Button, useDisclosure, Link,
+  Table, Thead, Tbody, Tr, Th, Td, TableCaption
 } from "@chakra-ui/react";
 
 const AirModal = ({ airQuality, particles }) => {
@@ -21,8 +22,80 @@ const AirModal = ({ airQuality, particles }) => {
           <ModalHeader>Air Quality Index</ModalHeader>
           <ModalCloseButton />
           <ModalBody>
-            Possible values: 1, 2, 3, 4, 5. Where 1 = Good, 2 = Fair, 3 = Moderate, 4 = Poor, 5 = Very Poor. <br />
-            Particles: co: {particles.co}
+            <Table variant="striped" colorScheme="green">
+              <TableCaption placement='top'>Possible values range from 1-5 where:</TableCaption>
+              <Thead>
+                <Tr>
+                  <Th>Value</Th>
+                  <Th>Rating</Th>
+                </Tr>
+              </Thead>
+              <Tbody>
+                <Tr>
+                  <Td>1</Td>
+                  <Td>Good</Td>
+                </Tr>
+                <Tr>
+                  <Td>2</Td>
+                  <Td>Fair</Td>
+                </Tr>
+                <Tr>
+                  <Td>3</Td>
+                  <Td>Moderate</Td>
+                </Tr>
+                <Tr>
+                  <Td>4</Td>
+                  <Td>Poor</Td>
+                </Tr>
+                <Tr>
+                  <Td>5</Td>
+                  <Td>Very Poor</Td>
+                </Tr>
+              </Tbody>
+            </Table>
+            <Table variant="striped" colorScheme="green">
+              <TableCaption placement='top'>Air quality is determined by the concentration of certain air pollutants, eight of which are listed below:</TableCaption>
+              <Thead>
+                <Tr>
+                  <Th>Particle</Th>
+                  <Th>Concentration</Th>
+                </Tr>
+              </Thead>
+              <Tbody>
+                <Tr>
+                  <Td>Carbon monoxide(CO)</Td>
+                  <Td>{particles.co} µg/m3</Td>
+                </Tr>
+                <Tr>
+                  <Td>Nitrogen monoxide(NO)</Td>
+                  <Td>{particles.no} µg/m3</Td>
+                </Tr>
+                <Tr>
+                  <Td>Nigrogen dioxide(No3)</Td>
+                  <Td>{particles.no2} µg/m3</Td>
+                </Tr>
+                <Tr>
+                  <Td>Ozone(O3)</Td>
+                  <Td>{particles.o3} µg/m3</Td>
+                </Tr>
+                <Tr>
+                  <Td>Sulpher dioxide(SO2)</Td>
+                  <Td>{particles.so2} µg/m3</Td>
+                </Tr>
+                <Tr>
+                  <Td>Fine particles matter(PM2.5)</Td>
+                  <Td>{particles.pm2_5} µg/m3</Td>
+                </Tr>
+                <Tr>
+                  <Td>Coarse particulate matter(PM10)</Td>
+                  <Td>{particles.pm10} µg/m3</Td>
+                </Tr>
+                <Tr>
+                  <Td>Ammonia(NH3)</Td>
+                  <Td>{particles.nh3} µg/m3</Td>
+                </Tr>
+              </Tbody>
+            </Table>
           </ModalBody>
 
           <ModalFooter>
