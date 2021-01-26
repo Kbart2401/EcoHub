5744b0af90079f20888d85e42051201d0082a740
Kyle Barthelmes <kbart2401@gmail.com>
2021-01-25 22:17:17 -0500

    stylized footer

--- frontend/src/components/sections/Footer.js
+++ frontend/src/components/sections/Footer.js
@@ -1,4 +1,6 @@
 import React from 'react';
+import { AiOutlineGithub, AiFillLinkedin } from 'react-icons/ai';
+import { FaAngellist } from 'react-icons/fa';
 import '../../stylesheets/footer.css';
 
 const Footer = () => {
@@ -6,22 +8,18 @@ const Footer = () => {
     <div className='footer-container'>
       <div className='footer_root'>
         <div className='footer_left'>
-          <span>Follow</span>
-          <div className='text'>View this project on Github</div>
-          <a className='linkText' href='https://github.com/Kbart2401/Catch-me-if-you-can' target='_blank'>
-            {/* <GitHubIcon /> */}
+          <div className='footer-text'>Follow</div>
+          <a className='linkText' href='https://github.com/Kbart2401/EcoHub' target='_blank'>
+            <div className='text'><span>View this project on Github</span><AiOutlineGithub size='25' /></div>
           </a>
         </div>
         <div className='footer_middle'>
-          <span>About Us</span>
-          <div className='text'>Creators</div>
-          <div className='contactContainer'>
-            <div className='contact'>
-              <a className='linkText' href='https://www.linkedin.com/in/kyle-barthelmes-a5120b51/'>Kyle Barthelmes</a> &nbsp;
-            <a className='linkText' href='https://github.com/Kbart2401/Catch-me-if-you-can' target='_blank'>
-                {/* <GitHubIcon /> */}
-              </a>
-            </div>
+          <div className='footer-text'>Creator</div>
+          <div className='footer-creator-links'>
+            <a className='linkText' href='https://www.linkedin.com/in/kyle-barthelmes-a5120b51/'
+              target='_blank'><div className='text'><span>Kyle Barthelmes</span><AiFillLinkedin size='25' /></div>
+            </a>
+            <a href='https://angel.co/u/kyle-barthelmes' target='_blank'><FaAngellist size='25' /></a>
           </div>
         </div>
       </div>
