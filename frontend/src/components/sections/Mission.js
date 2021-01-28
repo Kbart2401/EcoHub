a43119247ba9d3ee0fca2bb991f61a2985313686
Kyle Barthelmes <kbart2401@gmail.com>
2021-01-28 18:31:54 -0500

    stylized mission portion of landing page

--- frontend/src/components/sections/Mission.js
+++ frontend/src/components/sections/Mission.js
@@ -8,7 +8,7 @@ const Mission = () => {
       <h1>Mission</h1>
       <div><span className='logo'><span id='one'>Eco</span><span id='two'>Hub</span>
         <span></span></span>&nbsp;is designed to be a part of (even if small) the solution
-        in helping preserve the Earth 
+        in helping preserve the Earth
       </div>
       <div>by creating more evironmentally-minded users.</div>
       <div>Here find a like-minded community doing their part in their everyday activities. </div>
@@ -16,6 +16,9 @@ const Mission = () => {
       <div>Report any environmental issues to the community</div>
       <div>Find and connect with other users</div>
       <div>Compete to see who of your friends are the top contributors!</div>
+      <div className='mission-image-wrapper'>
+        <img src={require("../../images/2d-leaf.png")} />
+      </div>
     </div>
   )
 
