83d61c21e58e565cf7c6a748520d17fedbd54d17
Kyle Barthelmes <kbart2401@gmail.com>
2021-03-24 13:29:14 -0400

    re-working landing page

--- frontend/src/components/layouts/Landing.js
+++ frontend/src/components/layouts/Landing.js
@@ -38,43 +38,42 @@ const Landing = () => {
   }, [])
 
   useEffect(() => {
-    debugger
     if (user && user.email) {
       return history.push('/home');
     }
   })
 
-
-
   return (
     <>
-      <Flex className='carousel-container' flexDirection='column'>
-        <AspectRatio maxW="1200px">
-          <Box height="400px" width='100vw' display='flex' justifyContent='center'>
-            <AliceCarousel autoPlay infinite autoPlayInterval="4000" animationType='fadeout' >
-              <Image src={require("../../images/boy.jpg")} />
-              <Image src={require("../../images/lake.jpg")} />
-              <Image src={require("../../images/leaf.jpg")} />
-              <Image src={require("../../images/nature.jpg")} />
-              <Image src={require("../../images/park.jpg")} />
-              <Image src={require("../../images/river.jpg")} />
-            </AliceCarousel>
-          </Box>
-        </AspectRatio>
-      </Flex>
-      <div className='lower-landing'>
-        <ul className='landing-list'>
-          <li className='header-1 invisible' ref={header1}>Contribute</li>
-          <li className='header-2 invisible' ref={header2}>Encourage</li>
-          <li className='header-3 invisible' ref={header3}>Inspire</li>
-        </ul>
-        <div className='landing-trees' ref={landingTrees}>
-          <img id="tree1" src={require('../../images/2d-tree1.png')} />
-          <img id="tree2" src={require('../../images/2d-tree2.png')} />
-          <img id="tree3" src={require('../../images/2d-tree3.png')} />
+      <section id='landing-1'>
+        {/* <Flex className='carousel-container' flexDirection='column'> */}
+        {/* <AspectRatio maxW="1200px"> */}
+        {/* <Box height="400px" width='100vw' display='flex' justifyContent='center'> */}
+        <AliceCarousel autoPlay infinite autoPlayInterval="4000" animationType='fadeout' >
+          <Image src={require("../../images/boy.jpg")} />
+          <Image src={require("../../images/lake.jpg")} />
+          <Image src={require("../../images/leaf.jpg")} />
+          <Image src={require("../../images/nature.jpg")} />
+          <Image src={require("../../images/park.jpg")} />
+          <Image src={require("../../images/river.jpg")} />
+        </AliceCarousel>
+        {/* </Box> */}
+        {/* </AspectRatio> */}
+        {/* </Flex> */}
+        <div className='lower-landing'>
+          <ul className='landing-list'>
+            <li className='header-1 invisible' ref={header1}>Contribute</li>
+            <li className='header-2 invisible' ref={header2}>Encourage</li>
+            <li className='header-3 invisible' ref={header3}>Inspire</li>
+          </ul>
+          <div className='landing-trees' ref={landingTrees}>
+            <img id="tree1" src={require('../../images/2d-tree1.png')} />
+            <img id="tree2" src={require('../../images/2d-tree2.png')} />
+            <img id="tree3" src={require('../../images/2d-tree3.png')} />
+          </div>
         </div>
-        <Mission />
-      </div>
+      </section>
+      <Mission />
     </>
   )
 }
